0ab9cdf79d895be6f32a5972d625fc5c075fe3e5
rajasekhar <rajasekhar11022@gmail.com>
2022-10-12 10:40:30 +0530

    springboot-mockito-project

---
+++ src/main/java/com/javatechie/spring/mockito/api/model/User.java
@@ -0,0 +1,54 @@
+package com.javatechie.spring.mockito.api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+	public User(int id, String name, int age, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+	@Id
+	private int id;
+	private String name;
+	private int age;
+	private String address;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
